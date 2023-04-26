d484a3c91e21ab219eff283e9ce6008cecc655b9
lsh <liushaohui@163.com>
2023-04-26 17:14:43 +0800

    tido

--- generate-ui/src/router/index.js
+++ generate-ui/src/router/index.js
@@ -37,7 +37,7 @@ export const constantRoutes = [
 
 export default new Router({
     base: process.env.VUE_APP_APP_NAME ? process.env.VUE_APP_APP_NAME : "/",
-    mode: 'history', // 去掉url中的#
+    mode: 'hash', // 去掉url中的#
     scrollBehavior: () => ({y: 0}),
     routes: constantRoutes
 })
---
+++ generate/src/main/java/com/aprilz/generate/controller/IndexController.java
@@ -0,0 +1,18 @@
+//package com.aprilz.generate.controller;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//
+///**
+// * @author Aprilz
+// * @date 2023/4/26-9:33
+// * @description 入口
+// */
+//@Controller
+//public class IndexController {
+//
+//    @GetMapping("/")
+//    public String index() {
+//        return "index";
+//    }
+//}
