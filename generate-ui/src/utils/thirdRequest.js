96643545370407e227ddc6e081028babd9536e90
lsh <liushaohui@163.com>
2024-06-05 16:59:18 +0800

    改目录

--- generate-ui/src/utils/thirdRequest.js
+++ generate-ui/src/utils/thirdRequest.js
@@ -8,7 +8,7 @@ const thirdService = axios.create({
     // axios中请求配置有baseURL选项，表示请求URL公共部分
     //baseURL: process.env.VUE_APP_BASE_API,
     // 超时
-    timeout: 300000,
+    timeout: 600000,
     // 禁用 Cookie 等信息
     withCredentials: false,
 })
